21d1e67fa74012134babdaf7fe4158d84ac84c7a
Seri Hussain <serijhussain@gmail.com>
2020-06-17 21:13:09 -0500

    add color descriptors

--- __tests__/backend/utils/colorTemperature.test.js
+++ __tests__/backend/utils/colorTemperature.test.js
@@ -6,7 +6,7 @@ import 'react-native';
 import {getColorTempGradient} from '../../../src/helpers/colorTemperature';
 
 // Note: test renderer must be required after react-native.
-//import renderer from 'react-test-renderer';
+import renderer from 'react-test-renderer';
 
 test('getColorTempGradient', () => {
   for (var i = -20; i < 121; i + 20) {
--- src/helpers/colorTemperature.js
+++ src/helpers/colorTemperature.js
@@ -2,29 +2,29 @@ import Geolocation from '@react-native-community/geolocation';
 
 const baseUrl = 'https://api.weather.gov/';
 
-const getColorTempGradient = temp => {
+const getColorTempGradient = temperature => {
   var textColor = '';
 
   //cools
-  if (temp < 0) {
-    textColor = '#00FFFF';
-  } else if (temp > 0 < 20) {
-    textColor = '#00CCFF';
-  } else if (temp > 21 < 32) {
-    textColor = '#0099FF';
-  } else if (temp > 33 < 55) {
-    textColor = '#009999';
+  if (temperature < 0) {
+    textColor = '#00FFFF'; //ice blue
+  } else if (temperature > 0 < 20) {
+    textColor = '#00CCFF'; //less icy blue? 
+  } else if (temperature > 21 < 32) {
+    textColor = '#0099FF'; // blue??
+  } else if (temperature > 33 < 55) {
+    textColor = '#009999'; //teal
   }
 
   //warms
-  else if (temp > 56 < 75) {
-    textColor = '#FFFF66';
-  } else if (temp > 76 < 85) {
-    textColor = '#FFCC66';
-  } else if (temp > 86 < 100) {
-    textColor = '#FF9966';
+  else if (temperature > 56 < 75) {
+    textColor = '#FFFF66'; //yellow
+  } else if (temperature > 76 < 85) {
+    textColor = '#FFCC66'; //orange
+  } else if (temperature > 86 < 100) {
+    textColor = '#FF9966'; // red orange?? 
   } else {
-    textColor = '#FF6666';
+    textColor = '#FF6666'; // red
   }
 
   return textColor;
